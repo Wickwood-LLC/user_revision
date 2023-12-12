55360611c911522cc7673353b70fbdc6d1bb70a7
Junaid PV <junu.pv@gmail.com>
2023-12-12 19:52:57 +0530

    Override prepareRow method to migrate correct revision of each field.

--- src/Plugin/migrate/source/d7/UserComplete.php
+++ src/Plugin/migrate/source/d7/UserComplete.php
@@ -4,6 +4,8 @@ namespace Drupal\user_revision\Plugin\migrate\source\d7;
 
 use Drupal\Core\Database\Query\SelectInterface;
 use Drupal\user\Plugin\migrate\source\d7\User;
+use Drupal\migrate\Row;
+use Drupal\migrate\Plugin\migrate\source\SourcePluginBase;
 
 /**
  * Drupal 7 all user revisions source, including translation revisions.
@@ -93,5 +95,69 @@ class UserComplete extends User {
    * {@inheritdoc}
    */
   protected function handleTranslations(SelectInterface $query) {}
+
+
+  /**
+   * {@inheritdoc}
+   * It has only change from the parent version. To call getFieldValues() with user revision id passed.
+   */
+  public function prepareRow(Row $row) {
+    $uid = $row->getSourceProperty('uid');
+    $vid = $row->getSourceProperty('vid');
+
+    $roles = $this->select('users_roles', 'ur')
+      ->fields('ur', ['rid'])
+      ->condition('ur.uid', $uid)
+      ->execute()
+      ->fetchCol();
+    $row->setSourceProperty('roles', $roles);
+
+    $row->setSourceProperty('data', unserialize($row->getSourceProperty('data') ?? ''));
+
+    // If this entity was translated using Entity Translation, we need to get
+    // its source language to get the field values in the right language.
+    // The translations will be migrated by the d7_user_entity_translation
+    // migration.
+    $entity_translatable = $this->isEntityTranslatable('user');
+    $source_language = $this->getEntityTranslationSourceLanguage('user', $uid);
+    $language = $entity_translatable && $source_language ? $source_language : $row->getSourceProperty('language');
+    $row->setSourceProperty('entity_language', $language);
+
+    // Get Field API field values.
+    foreach ($this->getFields('user') as $field_name => $field) {
+      // Ensure we're using the right language if the entity and the field are
+      // translatable.
+      $field_language = $entity_translatable && $field['translatable'] ? $language : NULL;
+      $row->setSourceProperty($field_name, $this->getFieldValues('user', $field_name, $uid, $vid, $field_language));
+    }
+
+    // Get profile field values. This code is lifted directly from the D6
+    // ProfileFieldValues plugin.
+    if ($this->getDatabase()->schema()->tableExists('profile_value')) {
+      $query = $this->select('profile_value', 'pv')
+        ->fields('pv', ['fid', 'value']);
+      $query->leftJoin('profile_field', 'pf', '[pf].[fid] = [pv].[fid]');
+      $query->fields('pf', ['name', 'type']);
+      $query->condition('uid', $row->getSourceProperty('uid'));
+      $results = $query->execute();
+
+      foreach ($results as $profile_value) {
+        if ($profile_value['type'] == 'date') {
+          $date = unserialize($profile_value['value']);
+          $date = date('Y-m-d', mktime(0, 0, 0, $date['month'], $date['day'], $date['year']));
+          $row->setSourceProperty($profile_value['name'], ['value' => $date]);
+        }
+        elseif ($profile_value['type'] == 'list') {
+          // Explode by newline and comma.
+          $row->setSourceProperty($profile_value['name'], preg_split("/[\r\n,]+/", $profile_value['value']));
+        }
+        else {
+          $row->setSourceProperty($profile_value['name'], [$profile_value['value']]);
+        }
+      }
+    }
+
+    return SourcePluginBase::prepareRow($row);
+  }
 
 }
